b42eab4d010750f6145402507b531250346765f0
Christophe Jollivet <christophe.jollivet@gmail.com>
2019-04-24 16:55:39 +0200

    move instantly from anchor to one meter in front of camera

---
+++ app/src/main/java/fr/jollivetc/demoanimation/CardNode.java
@@ -0,0 +1,35 @@
+package fr.jollivetc.demoanimation;
+
+import android.util.Log;
+import android.view.MotionEvent;
+
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.HitTestResult;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.math.Vector3;
+
+public class CardNode extends Node implements Node.OnTapListener {
+
+    private boolean isPresented;
+    private AnchorNode anchor;
+
+    public CardNode(AnchorNode anchor){
+        this.anchor = anchor;
+        isPresented = false;
+        setOnTapListener(this);
+        this.setParent(anchor);
+    }
+
+    @Override
+    public void onTap(HitTestResult hitTestResult, MotionEvent motionEvent) {
+        if (isPresented){
+            this.setParent(anchor);
+            this.setLocalPosition(new Vector3(0.0f,0.0f,0.0f));
+        }else{
+
+            this.setParent(getScene().getCamera());
+            this.setLocalPosition(new Vector3(0.0f, 0.0f, -1.0f));
+        }
+        this.isPresented = !this.isPresented;
+    }
+}
---
+++ app/src/main/java/fr/jollivetc/demoanimation/MainActivity.java
@@ -0,0 +1,54 @@
+package fr.jollivetc.demoanimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ViewRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArFragment arFragment;
+    private ViewRenderable card;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+
+        ViewRenderable.builder()
+                .setView(this, R.layout.card)
+                .build()
+                .thenAccept(
+                        (renderable) -> card = renderable)
+                .exceptionally(
+                        (throwable) -> {
+                            throw new AssertionError("Could not load plane card view.", throwable);
+                        });
+
+        arFragment.setOnTapArPlaneListener(
+                (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
+                    if (card == null) {
+                        return;
+                    }
+
+                    // Create the Anchor.
+                    Anchor anchor = hitResult.createAnchor();
+                    AnchorNode anchorNode = new AnchorNode(anchor);
+                    anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+                    // Create CardNode and attach to the Anchor.
+                    CardNode cardNode = new CardNode(anchorNode);
+                    //cardNode.setParent(anchorNode);
+                    cardNode.setRenderable(card);
+                });
+    }
+}
